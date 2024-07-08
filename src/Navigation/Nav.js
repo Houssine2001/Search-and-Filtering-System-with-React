f9d0df75e570a1053e838785f1a49e649147bd46
houssine125 <houssinebenarous48@gmail.com>
2024-07-08 13:24:56 +0200

    Finale

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -2,31 +2,31 @@ import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 import "./Nav.css";
 
-function Nav() {
+const Nav = ({ handleInputChange, query }) => {
   return (
     <nav>
       <div className="nav-container">
-        <input type="text" placeholder="Enter your search shoes." />
+        <input
+          className="search-input"
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Enter your search shoes."
+        />
       </div>
-
       <div className="profile-container">
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#heart" className="nav-icon-link">
-          <FiHeart className="heart-icon" />
+        <a href="#">
+          <FiHeart className="nav-icons" />
         </a>
-
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#cart" className="nav-icon-link">
+        <a href="">
           <AiOutlineShoppingCart className="nav-icons" />
         </a>
-
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#user" className="nav-icon-link">
+        <a href="">
           <AiOutlineUserAdd className="nav-icons" />
         </a>
       </div>
     </nav>
   );
-}
+};
 
 export default Nav;
